4220054029125bd54760a343c5590d80d3b11c91
151250051 <151250051@smail.nju.edu.cn>
2018-06-03 23:38:05 +0800

    选课结构

--- web/resource/systemJS/form.js
+++ web/resource/systemJS/form.js
@@ -8,6 +8,20 @@ function load() {
         url: '/B_Subject',
         success: function (result) {
             console.log(result);
+            for (let i = 0; i < result.length; i++) {
+                let course = result[i];
+                $('#college').append(
+                    '<tr>' +
+                    '<td>' + course.id + '</td>' +
+                    '<td>' + course.name + '</td>' +
+                    '<td>' + course.teacher + '</td>' +
+                    '<td>' + course.time + '</td>' +
+                    '<td>' + course.location + '</td>' +
+                    '<td><button class="btn btn-link">选课</button></td>' +
+                    '</tr>'
+                );
+            }
+
         },
         error: function (xhr, status, error) {
             console.log(xhr.status);
