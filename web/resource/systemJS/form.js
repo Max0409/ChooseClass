564382b6dbde11255e7054704c3770fc4b13b11d
151250051 <151250051@smail.nju.edu.cn>
2018-06-04 15:23:44 +0800

    登录功能实现

--- web/resource/systemJS/form.js
+++ web/resource/systemJS/form.js
@@ -17,8 +17,14 @@ function load() {
                     '<td>' + course.teacher + '</td>' +
                     '<td>' + course.time + '</td>' +
                     '<td>' + course.location + '</td>' +
-                    '<td><button class="btn btn-link">选课</button></td>' +
-                    '</tr>'
+                    '<td><button class="btn btn-link" id="choose_'+ i +'">选课</button></td>' +
+                    '</tr>' +
+                    '<script>' +
+                    'let s_id = $("#user").val();' +
+                    '$("#choose_"' + i + ').click(function() {' +
+                    'chooseClass(s_id, course.id)' +
+                    '});' +
+                    '</script>'
                 );
             }
 
@@ -27,5 +33,22 @@ function load() {
             console.log(xhr.status);
         }
 
+    })
+}
+
+function chooseClass(s_id, c_id) {
+    $.ajax({
+        type: 'POST',
+        url: '/chooseSubject',
+        data: {
+            s_id: s_id,
+            c_id: c_id
+        },
+        success: function (result) {
+
+        },
+        error: function (xhr, status, error) {
+            console.log(xhr.status);
+        }
     })
 }
---
+++ web/resource/systemJS/index.js
@@ -0,0 +1,9 @@
+
+$('#person_info_btn').click(function () {
+
+    document.getElementById("view").innerText = "我的信息";
+
+    $('#person_info').show();
+    $('#course_table').hide();
+
+});
