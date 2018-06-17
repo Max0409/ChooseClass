3d9e074442613207320de34444335e07af438e63
151250051 <151250051@smail.nju.edu.cn>
2018-06-18 01:14:47 +0800

    管理员加载课程

--- src/main/resources/static/systemJS/form.js
+++ src/main/resources/static/systemJS/form.js
@@ -9,6 +9,7 @@ $('#own').click(function () {
     $('#other').removeClass("active-menu");
     $('#view').text("专业选课");
     $('#share').hide();
+    $('#college').empty();
     load();
 });
 
@@ -23,7 +24,8 @@ $('#other').click(function () {
 $('#check').click(function () {
     let dep = $('input[name=dep]:checked').val();
 
-    // loadShareCourse(dep);
+    getShareCourses(dep);
+
 });
 
 //加载所有课程信息
@@ -31,6 +33,67 @@ function load() {
     $.ajax({
         type: 'GET',
         url: '/user/B_Subject',
+        data: {
+            s_id: localStorage.getItem("user"),
+        },
+        success: function (result) {
+            let s_id = $("#user").val();
+            let resultList = parseXML(result).getElementsByTagName("b:课程");
+            for (let i = 0; i < resultList.length; i++) {
+                let c_id = resultList[i].getElementsByTagName("b:编号")[0].firstChild.nodeValue;
+                if (checkChoice(s_id, c_id)) {
+                    $('#college').append(
+                        '<tr>' +
+                        '<td>' + resultList[i].getElementsByTagName("b:编号")[0].firstChild.nodeValue + '</td>' +
+                        '<td>' + resultList[i].getElementsByTagName("b:名称")[0].firstChild.nodeValue + '</td>' +
+                        '<td>' + resultList[i].getElementsByTagName("b:课时")[0].firstChild.nodeValue + '</td>' +
+                        '<td>' + resultList[i].getElementsByTagName("b:学分")[0].firstChild.nodeValue + '</td>' +
+                        '<td>' + resultList[i].getElementsByTagName("b:老师")[0].firstChild.nodeValue + '</td>' +
+                        '<td>' + resultList[i].getElementsByTagName("b:地点")[0].firstChild.nodeValue + '</td>' +
+                        '<td>' +
+                        '<label id="chosen_' + i + '">已选择</label>' +
+                        '</td>' +
+                        '</tr>'
+                    );
+                } else {
+                    $('#college').append(
+                        '<tr>' +
+                        '<td>' + resultList[i].getElementsByTagName("b:编号")[0].firstChild.nodeValue + '</td>' +
+                        '<td>' + resultList[i].getElementsByTagName("b:名称")[0].firstChild.nodeValue + '</td>' +
+                        '<td>' + resultList[i].getElementsByTagName("b:课时")[0].firstChild.nodeValue + '</td>' +
+                        '<td>' + resultList[i].getElementsByTagName("b:学分")[0].firstChild.nodeValue + '</td>' +
+                        '<td>' + resultList[i].getElementsByTagName("b:老师")[0].firstChild.nodeValue + '</td>' +
+                        '<td>' + resultList[i].getElementsByTagName("b:地点")[0].firstChild.nodeValue + '</td>' +
+                        '<td>' +
+                        '<button class="btn btn-link" id="choose_' + i + '">选课</button>' +
+                        '</td>' +
+                        '</tr>' +
+                        '<script>' +
+                        '$("#choose_' + i + '").click(function() {' +
+                        'chooseClass("' + resultList[i].getElementsByTagName("b:编号")[0].firstChild.nodeValue + '")' +
+                        '});' +
+                        '</script>'
+                    );
+                }
+            }
+        },
+        error: function (xhr, status, error) {
+            console.log(xhr.status);
+        }
+
+    })
+}
+
+
+//获取共享课程
+function getShareCourses(dep) {
+    $.ajax({
+        type: 'GET',
+        url: '/user/getUserShareCourses',
+        data: {
+            sId: localStorage.getItem("user"),
+            dep: dep
+        },
         success: function (result) {
             let s_id = $("#user").val();
             let resultList = parseXML(result).getElementsByTagName("b:课程");
@@ -83,16 +146,8 @@ function load() {
 function chooseClass(c_id) {
     let s_id = $("#user").val();
     $.ajax({
-<<<<<<< HEAD
-<<<<<<< HEAD
         type: 'POST',
         url: '/user/chooseCourse',
-=======
-=======
->>>>>>> 5f73e55d2e0aaaaf4b36baadb2346c07c0e8f6d2
-        type: 'GET',
-        url: '/user/B_Subject',
->>>>>>> 5f73e55d2e0aaaaf4b36baadb2346c07c0e8f6d2
         data: {
             sId: s_id,
             cId: c_id
